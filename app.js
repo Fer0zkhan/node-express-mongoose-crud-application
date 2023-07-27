613cb190f0ff1b12748c656f1ed682b6be2ab889
Feroz Khan <50706350+Fer0zkhan@users.noreply.github.com>
2023-07-27 15:38:03 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-//Third Party MiddleWares
+//Third Party MiddleWare
 app.use(bodyParser.json());
 
 // Import Routes
