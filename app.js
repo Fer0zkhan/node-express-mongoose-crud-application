df48a3a7642b0e411dafa5176da38fcb56bc2869
Feroz Khan <50706350+Fer0zkhan@users.noreply.github.com>
2023-09-29 14:51:55 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 //Middleware
 app.use(bodyParser.json());
 
+
 // Import Routes
 const postData = require('./routes/postData');
 const getData = require('./routes/getData');
